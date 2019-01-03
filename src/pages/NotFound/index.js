e41c1763e92d1acd5cf7557cdb32a41a3cdf8cfc
Jon Preece <jonpreece@hotmail.co.uk>
2019-01-03 09:53:07 +0000

    Tidy up 404 page

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -3,11 +3,21 @@ import { Link } from '@reach/router'
 
 function NotFound() {
   return (
-    <>
-      <h1>404.</h1>
-      <p>Sorry the requested page was not found</p>
-      <Link to="/">Home</Link>
-    </>
+    <div className="columns">
+      <div className="column col-12">
+        <div className="empty">
+          <div className="empty-icon">
+            <i className="icon icon-3x icon-mail" />
+          </div>
+          <p className="empty-title h5">404.</p>
+          <p className="empty-subtitle">Sorry the requested page was not found</p>
+          <div className="empty-action">
+            {' '}
+            <Link to="/">Go back home</Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
